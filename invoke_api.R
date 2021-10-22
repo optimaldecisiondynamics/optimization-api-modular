0c04b1721ab881ce6fcf14c9ffe2600b76ebb58b
optimaldecisiondynamics <>
2021-10-21 22:01:30 -0400

    Update the invoke_api code to show the invocations locally as well as on cloud run.

--- invoke_api.R
+++ invoke_api.R
@@ -1,6 +1,22 @@
 library(httr)
 library(dplyr)
 
+# When running locally
+# The port (8080 in this case) must be part of the url
 hiking_test <- POST("http://127.0.0.1:8080/mathematical_hiking",
                   body = list(param = upload_file("hiking_inputs.zip")),
                   write_disk("hiking_output.zip", overwrite = TRUE))
+
+# When running on cloud run the port should NOT be included in the url
+# E.g., https://your_api_url/mathematical_hiking
+
+# 2 API requests, one for each model type
+hiking_test <- POST("https://your_api_url/mathematical_hiking/mathematical_hiking",
+                    body = list(param = upload_file("hiking_inputs.zip")),
+                    query = list(model_type = "min_max_elevation_change"),
+                    write_disk("hiking_output.zip", overwrite = TRUE))
+
+hiking_test2 <- POST("https://your_api_url/mathematical_hiking/mathematical_hiking",
+                    body = list(param = upload_file("hiking_inputs.zip")),
+                    query = list(model_type = "shortest_path"),
+                    write_disk("hiking_output.zip", overwrite = TRUE))
